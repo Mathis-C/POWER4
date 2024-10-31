0227e8e6de8be2859e7f702488e31a504c6e6ac7
Mathis-C <m.collette@elisa-aerospace.fr>
2024-10-31 12:17:57 +0100

    Update MENU_GRID_2.h

--- MENU_GRID_2.h
+++ MENU_GRID_2.h
@@ -15,12 +15,19 @@ public:
     explicit MENU_GRID_2(QWidget *parent = nullptr);
     void tab2P();
 
+private slots :
+
+    void GRID_8();
+    void GRID_12();
+
 private:
 
     QLabel * tailletab;
     QPushButton * TAB1;
     QPushButton * TAB2;
     QPushButton * Quit1;
+    PLAY_2P_8 * P2_8;
+    PLAY_2P_12 * P2_12;
 
 };
 
