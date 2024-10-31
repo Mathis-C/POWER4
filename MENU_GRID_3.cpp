68e7e31081753e6c07c97993afe99cc0391a1898
Mathis-C <m.collette@elisa-aerospace.fr>
2024-10-31 11:15:31 +0100

    Update MENU_GRID_3.cpp

--- MENU_GRID_3.cpp
+++ MENU_GRID_3.cpp
@@ -17,6 +17,11 @@ void MENU_GRID_3:: tab3P(){
     tailletab = new QLabel ("SELECT GRID SIZE", this);
     TAB1 = new QPushButton (" 8x8 GRID", this);
     TAB2 = new QPushButton ("12x12 GRID", this);
+    Quit1 = new QPushButton ( " QUIT " , this);
+
+    connect(Quit1, SIGNAL(clicked()), this, SLOT(close()));
+
+
 
     QHBoxLayout * titre = new QHBoxLayout();
     titre -> addStretch();
@@ -29,12 +34,18 @@ void MENU_GRID_3:: tab3P(){
     QHBoxLayout * t2 = new QHBoxLayout();
     t2 -> addWidget(TAB2);
 
+    QHBoxLayout * QUIT = new QHBoxLayout();
+    QUIT -> addWidget(Quit1);
+
     QVBoxLayout * MENUG3P = new QVBoxLayout(this);
     MENUG3P -> addLayout(titre);
     MENUG3P -> addLayout(t1);
     MENUG3P -> addLayout(t2);
+    MENUG3P -> addLayout(QUIT);
+
 
     setLayout(MENUG3P);
 
 
 }
+
