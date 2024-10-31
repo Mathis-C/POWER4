55f2658e66bea2a09721683a16b7a5e74c96ac58
Mathis-C <m.collette@elisa-aerospace.fr>
2024-10-31 11:01:16 +0100

    Update MENU_GRID_2.h

--- MENU_GRID_2.h
+++ MENU_GRID_2.h
@@ -1,1 +1,25 @@
-coucccccc
+#ifndef MENU_GRID_2_H
+#define MENU_GRID_2_H
+#include <QMainWindow>
+#include <QWidget>
+#include <QComboBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QDoubleValidator>
+
+class MENU_GRID_2 : public QWidget
+{
+    Q_OBJECT
+public:
+
+    explicit MENU_GRID_2(QWidget *parent = nullptr);
+    void tab2P();
+
+private:
+
+    QLabel * tailletab;
+    QPushButton * TAB1;
+    QPushButton * TAB2;
+};
+
+#endif // MENU_GRID_2_H
