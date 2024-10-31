21cea3961028de0ee21218bb30820610d2dee97c
Mathis-C <m.collette@elisa-aerospace.fr>
2024-10-31 11:00:31 +0100

    Update MENU_PLAYER.cpp

--- MENU_PLAYER.cpp
+++ MENU_PLAYER.cpp
@@ -1,11 +1,12 @@
 #include "menu_player.h"
+#include "menu_grid_2.h"
+#include "menu_grid_3.h"
 #include "qobjectdefs.h"
 #include <QMainWindow>
 #include <QWidget>
 #include <QComboBox>
 #include <QPushButton>
 #include <QLabel>
-#include <QDoubleValidator>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 
@@ -17,12 +18,15 @@ void MENU_PLAYER:: start(){
 
     nbrJ = new QLabel (" SELECT NUMBERS OF PLAYERS ", this);
     TwoP = new QPushButton ("2 PLAYERS" , this);
-    ThreeP = new QPushButton (" 3 PLAYERSY" , this);
+    ThreeP = new QPushButton (" 3 PLAYERS" , this);
     Quit1 = new QPushButton ( " QUIT " , this);
     
+
     connect(Quit1, SIGNAL(clicked()), this, SLOT(close()));
     connect(TwoP, SIGNAL(clicked()), this, SLOT(GRID_2P()));
     connect(ThreeP, SIGNAL(clicked()), this, SLOT(GRID_3P()));
+
+
 
     QHBoxLayout * titreP = new QHBoxLayout();
     titreP->addStretch();
@@ -35,34 +39,36 @@ void MENU_PLAYER:: start(){
     QHBoxLayout * THREE_PLAYER = new QHBoxLayout();
     THREE_PLAYER -> addWidget(ThreeP);
 
-    QHBoxLayout * RETURN = new QHBoxLayout();
-    RETURN -> addWidget(return1);
+    QHBoxLayout * QUIT = new QHBoxLayout();
+    QUIT -> addWidget(Quit1);
 
     QVBoxLayout * MENUP = new QVBoxLayout(this);
 
     MENUP -> addLayout(titreP);
     MENUP -> addLayout(TWO_PLAYER);
     MENUP -> addLayout(THREE_PLAYER);
-    MENUP -> addLayout(RETURN);
+    MENUP -> addLayout(QUIT);
 
     setLayout(MENUP);
 
 }
 
 void MENU_PLAYER:: GRID_2P(){
-    
+
     MENU_GRID_2 * menu2P = new MENU_GRID_2;
     menu2P -> show();
     menu2P -> tab2P();
-    
-}
 
+}
 
 
 void MENU_PLAYER:: GRID_3P(){
-    
+
     MENU_GRID_3 * menu3P = new MENU_GRID_3;
     menu3P -> show();
     menu3P -> tab3P();
-    
+
 }
+
+
+
